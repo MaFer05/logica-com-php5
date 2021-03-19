e58f6ca41721b4d06c172ea868e93ddd57ed5002
MaFer05 <mariafernanda.05.oliveira@gmail.com>
2021-03-19 11:34:48 +0000

    Estruturas de repetição com Vetores

---
+++ exemplo.php
@@ -0,0 +1,32 @@
+<?php
+$vet1 = array();
+$vet2 = [];
+
+$vet3 = array('maça', 'mamão', 'melancia');
+$vet4 = ['tomate', 'abóbora', 'cenoura'];
+
+$vet5 = ['nome' => 'Maria', 'idade' => 16, 'cidade' => 'Ibirarema'];
+
+$vet3[]= 'morango';
+
+echo "Usando FOR\n";
+for($i = 0;$i < count($vet3);$i++) {
+    echo $i . "\t:\t" . $vet3[$i] . "\n";
+}
+
+echo "Usando FOREACH\n";
+foreach($vet4 as $indice => $item) {
+    echo $indice . "\t:\t" . $item . "\n";
+}
+
+$alunos = [
+    ['nome' => 'José', 'idade' => 15],
+    ['nome' => 'Maria', 'idade' => 16],
+    ['nome' => 'Antônio', 'idade' => 15],
+];
+
+foreach($alunos as $a) {
+    foreach($a as $campo => $valor) {
+        echo $campo . "\t:\t" . $valor . "\n";
+    }
+}
